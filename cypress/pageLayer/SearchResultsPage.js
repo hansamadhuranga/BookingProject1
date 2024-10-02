c40eba53318ae44080325a3764efc9245ed60e1e
Hansa <h.a.h.maduranga@mail.com>
2024-10-02 20:05:11 +0530

    ChangePOM

--- cypress/pageLayer/SearchResultsPage.js
+++ cypress/pageLayer/SearchResultsPage.js
@@ -1,5 +1,15 @@
 class SearchResultsPage {
 
+    elements={
+        rateFilterGroup: () => cy.get('div[data-filters-group="class"]'),
+        rateFilterClass: (rating) => cy.get(`input[data-filters-item="class:class=${rating}"]`),    
+        priceFilter: () => cy.get('a[data-type="price"]'),  
+        propertyCard: () => cy.get('div[data-testid="property-card"]'),
+        secondItemTitle: () => cy.get('div[data-testid="property-card"]').eq(1).find('div[data-testid="title"]'),
+        secondItemPrice: () => cy.get('div[data-testid="property-card"]').eq(1).find('span[data-testid="price-and-discounted-price"]'),
+    }  
+        
+
     filterByRating(rating) {
         cy.get('div[data-filters-group="class"]').click();
         cy.get(`input[data-filters-item="class:class=${rating}"]`).click(); // Example: 3 stars
---
+++ cypress/testLayer/SearchResultsTest.js
@@ -0,0 +1,26 @@
+import SearchResultsPage from "../pageLayer/SearchResultsPage";
+
+class HomeTest{
+    constructor(){
+        this.SearchResultsPage = new SearchResultsPage;
+    }
+
+ratingAndFilter(){
+this.SearchResultsPage.filterByRating
+this.SearchResultsPage.sortByPrice
+this.SearchResultsPage.selectSecondItem
+this.SearchResultsPage.getSecondItemName
+this.SearchResultsPage.getSecondItemPrice
+this.SearchResultsPage.clickSecondItem
+
+ }   
+
+
+
+
+
+
+
+
+
+}
